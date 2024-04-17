5beee92b27fac9bed8da23bc92e2d63d72e508b6
GeorgeKonrad29 <jorge.lopez2@utp.edu.co>
2024-04-17 11:01:13 -0500

    Empezando Logica

--- HashTable.cpp
+++ HashTable.cpp
@@ -1,8 +1,57 @@
 #include <iostream>
 #include <math.h>
+#include <functional>
+#include <list>
+using std::cout;
 
-using std::cout ;
+template <typename T,typename U> class HashTable {
+Private:
+    class node
+    {
+    private:
+        T* key;
+        U* data;
+    public:
+        node(): key=nullptr, data=nullptr;{}
+        node(T& newKey, U& newData)
+        {
+            key= newKey;
+            data= newData;
+        }
+        U& getData const()
+        {
+            return data;
+        }
+        T& getKey const()
+        {
+            return key;
+        }
 
-template <class T> class HashTable {
+    };
+    int KeyNumber;
+    int dataArraySize;
+    list<vector> range[];
     
+    int hashFunction()
+    {
+        int hNumber =  keyNumber mod dataArraySize;
+        return hNumber;
+    }
+public:
+    HashTable() : keyNumber=0, dataArraySize=10, DataArray[10];{}
+    insert(const T& key, const U& data)
+    {
+        int hashNumber;
+        keyNumber++;
+        hashNumber = hashFuntion();
+        //crear lista con datos
+        list<node> bucket;
+        //crear un nodo por medio del constructor
+        node* newInput= new node(key,data);
+        list.pushBack(node);
+
+        vector<T> key;
+
+ 
+     }
 };
